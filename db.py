4d64c50d392282b8b2e0a088aeceb33853b35809
diksha <dikshakewat3776@gmail.com>
2024-10-26 10:07:48 +0530

    Freezing updated changes

--- db.py
+++ db.py
@@ -34,23 +34,3 @@ def get_conversations(user_id):
     # Return the last conversation or an empty list if none exists
     print("fetched the conversation: ", str(last_conversation))
     return last_conversation if last_conversation else []
-    #     return [{
-    #   "_id": {
-    #     "$oid": "671c20f5269b481ca33aa98b"
-    #   },
-    #   "user_id": {
-    #     "$numberLong": "1729896674718"
-    #   },
-    #   "user_input": "How old are you?",
-    #   "bot_response": {
-    #     "Hello! I’m PersonaWiseAI, your personal financial assistant. To help you build a tailored financial profile, please share your financial and demographic information.": "ok",
-    #     "What’s your monthly income?": "30000",
-    #     "What are your monthly expenses?": "2000",
-    #     "Do you have any assets? (e.g., property, savings)": "1 property",
-    #     "What are your liabilities? (e.g., loans, debts)": "3 loans",
-    #     "How old are you?": "29"
-    #   },
-    #   "timestamp": {
-    #     "$date": "2024-10-26T04:21:33.385Z"
-    #   }
-    # }]
